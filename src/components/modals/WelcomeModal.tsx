dededd939d23811559b24931596d52595b9b7af8
wednesdaywoe <wednesday.woe@gmail.com>
2026-04-06 11:33:42 -0400

    fix: update Discord invite link in FeedbackModal component

--- src/components/modals/WelcomeModal.tsx
+++ src/components/modals/WelcomeModal.tsx
@@ -92,7 +92,7 @@ export function WelcomeModal({ isOpen, onClose }: WelcomeModalProps) {
               </a>
               {' '}or join the{' '}
               <a
-                href="https://discord.gg/tJKAsMxK"
+                href="https://discord.gg/TeTtvydZ"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-amber-300 underline hover:text-amber-100"
